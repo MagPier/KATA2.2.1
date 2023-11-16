2ca589f599436662d1e32861c8c836d6f1a06da3
Magpier <garson.ao@gmail.com>
2023-11-16 18:05:18 +0300

    Решение задания версия 1

--- 2.2.1. spring_hibernate/src/main/java/hiber/MainApp.java
+++ 2.2.1. spring_hibernate/src/main/java/hiber/MainApp.java
@@ -1,6 +1,7 @@
 package hiber;
 
 import hiber.config.AppConfig;
+import hiber.model.Car;
 import hiber.model.User;
 import hiber.service.UserService;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
@@ -13,12 +14,19 @@ public class MainApp {
       AnnotationConfigApplicationContext context = 
             new AnnotationConfigApplicationContext(AppConfig.class);
 
+      Car car1 = new Car("Toyota Corolla", 2021);
+      Car car2 = new Car("Mersedes GLE", 2009);
+      Car car3 = new Car("Audi Q7", 2020);
+      Car car4 = new Car("Subaru Outback", 2008);
+
+
+
       UserService userService = context.getBean(UserService.class);
 
-      userService.add(new User("User1", "Lastname1", "user1@mail.ru"));
-      userService.add(new User("User2", "Lastname2", "user2@mail.ru"));
-      userService.add(new User("User3", "Lastname3", "user3@mail.ru"));
-      userService.add(new User("User4", "Lastname4", "user4@mail.ru"));
+      userService.add(new User("User1", "Lastname1", "user1@mail.ru",car1));
+      userService.add(new User("User2", "Lastname2", "user2@mail.ru",car2));
+      userService.add(new User("User3", "Lastname3", "user3@mail.ru",car3));
+      userService.add(new User("User4", "Lastname4", "user4@mail.ru",car4));
 
       List<User> users = userService.listUsers();
       for (User user : users) {
@@ -26,8 +34,14 @@ public class MainApp {
          System.out.println("First Name = "+user.getFirstName());
          System.out.println("Last Name = "+user.getLastName());
          System.out.println("Email = "+user.getEmail());
+         System.out.println("Car ="+user.getCar());
          System.out.println();
       }
+      for (User user : userService.listUsers()) {
+         System.out.println(user + " " + user.getCar());
+         System.out.println();
+      }
+      System.out.println(userService.getUserByCar("Mersedes GLE", 2009));
 
       context.close();
    }
--- 2.2.1. spring_hibernate/src/main/java/hiber/dao/UserDao.java
+++ 2.2.1. spring_hibernate/src/main/java/hiber/dao/UserDao.java
@@ -4,7 +4,13 @@ import hiber.model.User;
 
 import java.util.List;
 
+import hiber.model.User;
+import java.util.List;
+
 public interface UserDao {
    void add(User user);
+
    List<User> listUsers();
+
+   User getUserByCar(String model, int series);
 }
