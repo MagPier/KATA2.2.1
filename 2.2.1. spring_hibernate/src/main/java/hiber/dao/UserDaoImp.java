24d26e745b19cbd93964e8eba31d97048342d42e
Magpier <garson.ao@gmail.com>
2023-11-18 17:16:48 +0300

    Решение задания версия 4 Убрал непараметризированный квери.

--- 2.2.1. spring_hibernate/src/main/java/hiber/dao/UserDaoImp.java
+++ 2.2.1. spring_hibernate/src/main/java/hiber/dao/UserDaoImp.java
@@ -1,38 +1,48 @@
 package hiber.dao;
 
 import hiber.model.User;
+import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.springframework.stereotype.Repository;
 
+import javax.persistence.Query;
 import javax.persistence.TypedQuery;
+import javax.persistence.criteria.CriteriaBuilder;
+import javax.persistence.criteria.CriteriaQuery;
+import javax.persistence.criteria.Root;
 import java.util.List;
 
 @Repository
 public class UserDaoImp implements UserDao {
 
-   private final SessionFactory sessionFactory;
+    private final SessionFactory sessionFactory;
 
-   public UserDaoImp(SessionFactory sessionFactory) {
-      this.sessionFactory = sessionFactory;
-   }
+    public UserDaoImp(SessionFactory sessionFactory) {
+        this.sessionFactory = sessionFactory;
+    }
 
-   @Override
-   public void add(User user) {
-      sessionFactory.getCurrentSession().save(user);
-   }
+    @Override
+    public void add(User user) {
+        sessionFactory.getCurrentSession().save(user);
+    }
 
-   @Override
-   public List<User> listUsers() {
-      String hql = "from User";
-      TypedQuery<User> query = sessionFactory.getCurrentSession().createQuery(hql);
-      return query.getResultList();
-   }
+    @Override
+    public List<User> listUsers() {
+        Session session = sessionFactory.getCurrentSession();
+        CriteriaBuilder cb = sessionFactory.getCriteriaBuilder();
+        CriteriaQuery<User> criteriaQuery = cb.createQuery(User.class);
+        Root<User> root = criteriaQuery.from(User.class);
+        criteriaQuery.select(root);
+        Query query = session.createQuery(criteriaQuery);
+        return query.getResultList();
+    }
 
-   @Override
-   public User getUserByCar(String model, int series) {
-      String hql = "from User user where user.car.model = :model and user.car.series = :series";
-      TypedQuery<User> query = sessionFactory.getCurrentSession().createQuery(hql);
-      query.setParameter("model", model).setParameter("series", series);
-      return query.setMaxResults(1).getSingleResult();
-   }
+    @Override
+    public User getUserByCar(String model, int series) {
+        String hql = "from User user where user.car.model = :model and user.car.series = :series";
+        TypedQuery<User> query = sessionFactory.getCurrentSession().createQuery(hql);
+        query.setParameter("model", model)
+                .setParameter("series", series);
+        return query.setMaxResults(1).getSingleResult();
+    }
 }
